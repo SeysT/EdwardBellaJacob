784b1c633c2bac714127ae2f3820a9d87588b237
Guillaume <gllmcornet@gmail.com>
2018-01-26 12:44:44 +0100

    added PlayerServerTrame class

---
+++ IA/PlayerServerTrame.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IA
+{
+enum HeaderPlayerIA
+    {
+        NME,
+        MOV
+    }
+
+    class PlayerServerTrame
+    {
+        string s_header; // length 3
+        string s_size; // 1 Byte int
+        string s_payload; 
+    }
+}
