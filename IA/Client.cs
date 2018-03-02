a7a3d2e6140bd6babe0c19c9b90e95eadd59e4b5
Thibaut Seys <thibaut.seys@student.ecp.fr>
2018-03-02 12:32:47 +0100

    update client

--- IA/Client.cs
+++ IA/Client.cs
@@ -27,6 +27,17 @@ namespace IA
             this._socket.Connect(this._host, this._port);
 
             new NMETrame(this._playerName).Send(this._socket);
+
+            new SETTrame().Receive(this._socket);
+            new HUMTrame().Receive(this._socket);
+            new HMETrame().Receive(this._socket);
+            new MAPTrame().Receive(this._socket);
+            new UPDTrame().Receive(this._socket);
+
+            while (true)
+            {
+                new UPDTrame().Receive(this._socket);
+            }
         }
     }
 }
