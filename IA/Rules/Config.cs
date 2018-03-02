32a14c7d99cee4c8d6babeb812a9d5d540b270cf
yaay13 <yay13@live.fr>
2018-03-02 13:47:18 +0100

    Config + Board

--- IA/Rules/Config.cs
+++ IA/Rules/Config.cs
@@ -4,13 +4,11 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace IA
+namespace IA.Rules
 {
-    class Program
+    class Config
     {
-        static void Main(string[] args)
-        {
-            
-        }
+        //'n' pour nous , 'v' pour vous et 'h' pour hommes 
     }
+
 }
