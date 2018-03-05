d176e4b9b1e845cb1733cff8acfb11c5cec9c7cc
Thibaut Seys <thibaut.seys@student.ecp.fr>
2018-03-05 16:22:32 +0100

    make Start() acting like a loop

--- IA/Client.cs
+++ IA/Client.cs
@@ -1,9 +1,7 @@
-﻿using IA.Trame;
-using System;
-using System.Net.Sockets;
-using System.Text;
+﻿using System.Net.Sockets;
 using System.Threading;
 using System.Diagnostics;
+using System;
 
 namespace IA
 {
@@ -13,9 +11,12 @@ namespace IA
         private int _port;
 
         private Socket _socket;
+        private Stopwatch _sw;
 
         private string _playerName = "EdwardBellaJacob";
         private BaseServerPlayerTrame _trame;
+
+        private int[,] _startPos;
 
         public Client(string host, int port)
         {
@@ -24,65 +25,75 @@ namespace IA
 
             this._socket = new Socket(SocketType.Stream, ProtocolType.Tcp);
             this._trame = new BaseServerPlayerTrame(this._socket);
+
+            this._sw = new Stopwatch();
+        }
+
+        private void _initGame() {
+            // First trame has already been received outside _initGame()
+            BaseServerPlayerTrame.CheckTrameType(this._trame, "SET");
+
+            this._trame.Receive();
+            BaseServerPlayerTrame.CheckTrameType(this._trame, "HUM");
+
+            this._startPos = this._trame.Receive();
+            BaseServerPlayerTrame.CheckTrameType(this._trame, "HME");
+
+            this._trame.Receive();
+            BaseServerPlayerTrame.CheckTrameType(this._trame, "MAP");
+        }
+
+        private int[, ] _chooseMove()
+        {
+            int[,] nextPos = (int[,]) this._startPos.Clone();
+            nextPos[0, 0] = nextPos[0, 0] - 1;
+            nextPos[1, 0] = nextPos[1, 0];
+
+            int[,] next = { { this._startPos[0, 0], this._startPos[1, 0], 4, nextPos[0, 0], nextPos[1, 0] } };
+
+            this._startPos = nextPos;
+            return next;
         }
 
         public void Start()
         {
             this._socket.Connect(this._host, this._port);
 
-
             new NMETrame(this._playerName).Send(this._socket);
 
             this._trame.Receive();
-            BaseServerPlayerTrame.CheckTrameType(this._trame, "SET");
+            this._initGame();
 
-            this._trame.Receive();
-            BaseServerPlayerTrame.CheckTrameType(this._trame, "HUM");
-
-            int[,] startPos = this._trame.Receive();
-            BaseServerPlayerTrame.CheckTrameType(this._trame, "HME");
-
-            this._trame.Receive();
-            BaseServerPlayerTrame.CheckTrameType(this._trame, "MAP");
-
-            this._trame.Receive();
-            BaseServerPlayerTrame.CheckTrameType(this._trame, "UPD");
-
-            int[, ] nextPos = new int[2, 1];
-            nextPos[0, 0] = startPos[0, 0];
-            nextPos[1, 0] = startPos[1, 0] - 1;
-
-            int[,] move = { { startPos[0, 0], startPos[1, 0], 4, nextPos[0, 0], nextPos[1, 0] } };
-
-            new MOVTrame(move).Send(this._socket);
-
-            while (true)
+            bool finish = false;
+            while (!finish)
             {
-                this._trame.Receive();
+                int[, ] received = this._trame.Receive();
 
-                Stopwatch sw = new Stopwatch();
-                sw.Start();
-
-                if (this._trame.TrameHeader != "UPD")
+                switch (this._trame.TrameHeader)
                 {
-                    Console.WriteLine(this._trame.TrameHeader);
-                    this._trame.Receive();
-                    Console.WriteLine(this._trame.TrameHeader);
-                    break;
+                    case "UPD":
+                        Thread.Sleep(1000);
+                        this._sw.Start();
+
+                        int[,] next = this._chooseMove();
+                        this._sw.Stop();
+                        Console.WriteLine($"Elapsed Time={this._sw.Elapsed}");
+
+                        new MOVTrame(next).Send(this._socket);
+                        break;
+                    case "END":
+                        this._trame.Receive();
+                        if (this._trame.TrameHeader == "BYE")
+                        {
+                            finish = true;
+                        } else
+                        {
+                            this._initGame();
+                        }
+                        break;
+                    default:
+                        break;
                 }
-                Thread.Sleep(1000);
-          
-                startPos = (int[, ]) nextPos.Clone();
-                nextPos[0, 0] = nextPos[0, 0] - 1;
-                nextPos[1, 0] = nextPos[1, 0];
-
-                int[,] next = {
-                    { startPos[0, 0], startPos[1, 0], 4, nextPos[0, 0], nextPos[1, 0] }
-                };
-
-                sw.Stop();
-                Console.WriteLine("Elapsed Time={0}", sw.Elapsed);
-                new MOVTrame(next).Send(this._socket);
             }
         }
     }
