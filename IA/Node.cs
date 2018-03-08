fa6eb5d7136e17012abe8ebbde76fe12db0936b8
Hyunwoo JANG <hyunwoo.jang@student.ecp.fr>
2018-03-08 15:05:33 +0100

    added first prototype of MinMax with Node class

---
+++ IA/Node.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+////////////////////////////////////////////////////////////////////////////////
+// class Node is used to implement the minimax algorithm.
+// it will enable us to store the tree explored by alpha beta algorithm
+// could be optimized, but was developped for debug purposes
+////////////////////////////////////////////////////////////////////////////////
+
+namespace IA
+{
+    class NodeData
+    {
+        public NodeData()
+        {
+            HeuristicScore = 0;
+            MinMaxScore = 0;
+        }
+
+        // stores current position
+        public float HeuristicScore { get; set; }
+        public float MinMaxScore { get; set; }      // stores min max score
+
+        public List<Move> Moves { get; set; }
+    }
+
+    class Node
+    {
+        public Node(NodeData NodeData)
+        {
+            Data = NodeData;
+            this.Children = new List<Node>();
+        }
+
+        public NodeData Data { get; private set; }
+        public List<Node> Children { get; private set; }
+
+        // Method checking if Node is Leaf
+        public bool IsLeaf()
+        {
+            return (Children.Count() == 0);
+        }
+    }
+}
